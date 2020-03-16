b550df5238fb57d77a7c2eb8b689196a113bf64a
Egor <supertramp2295@gmail.com>
2020-03-16 12:19:45 +0200

    Add TaskList component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
+import TaskList from "./TaskList";
 
 
 function Board() {
-
-
     return (
       <div>
           <Button variant="contained" color="primary">
               Board name
           </Button>
 
-          
+          <TaskList/>
 
           <div>List Creator</div>
       </div>
---
+++ src/components/TaskList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TaskList() {
+    return(
+        <div className='TaskList'>
+            <div className='Title'>Title</div>
+        </div>
+    )
+
+}
+
+export default TaskList
