79ea05bc1cd2aa35a1c17c8db11d7fa05f1067d9
Egor <supertramp2295@gmail.com>
2020-03-16 12:01:45 +0200

    Add Board.js file create Board component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Board from "./components/Board";
 
 class App extends Component {
   render() {
     return (
-        <div>
-
+        <div className='App-page'>
+            <Board/>
         </div>
     );
   }
---
+++ src/components/Board.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+
+
+function Board() {
+
+
+    return (
+      <div>
+          <Button variant="contained" color="primary">
+              Board name
+          </Button>
+
+          
+
+          <div>List Creator</div>
+      </div>
+    );
+}
+
+export default Board
+
+
+
+
