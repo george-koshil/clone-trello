38423d863fd1c33afefaff0a352f7adbd9236ffe
Egor <supertramp2295@gmail.com>
2020-03-19 11:50:21 +0200

    Add CardCreator component and style to him

--- src/components/CardCreator.js
+++ src/components/CardCreator.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "@material-ui/core/Button";
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
-
+import TextField from '@material-ui/core/TextField';
 
 
 function CardCreator() {
+    let [isCardInput, setIsCardInput] = useState(false);
+
+    if(isCardInput) {
+        return(
+            <div className='CardCreator'>
+            <TextField
+                id="outlined-textarea"
+                label="Input text"
+                multiline
+                variant="outlined"
+                fullWidth={true}
+            />
+            </div>
+        )
+    }
     return(
         <div className='CardCreator'>
             <Button
                 variant="contained"
                 color="primary"
                 startIcon={<PlaylistAddIcon/>}
+                fullWidth={true}
+                onClick={() => setIsCardInput(true)}
             >
-                    Add card
+                    Add new card
                 </Button>
+
+
 
         </div>
     );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -9,10 +9,7 @@ function TaskList() {
         <div className='TaskList'>
             <TaskListTitle title='Today'/>
             <hr />
-            <Card text='Create static component'/>
-            <Card text="Go to Sanya's job and training" />
-            <Card text='Kiss Alina and tell her how I love her' />
-            <Card text='Listen to music' />
+            
             <CardCreator/>
         </div>
     )
